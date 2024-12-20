b0a197fb91ac8c8d6dbe02530769c0d22a6614a1
Ящук Максим <maxim2005yaschuk@gmail.com>
2024-12-20 09:21:26 +0300

    [task_03] Add task solution (#224)
    * added first lab
    
    * added third lab
    
    * changed readme file
    
    * fix CMake
    
    * fix code
    
    * fixed lab_3
    
    * fixed lab3
    
    * sorted documents
    
    * cmake list fix attempt
    
    ---------
    
    Co-authored-by: Yana Sitkovets <yana.sitkowecz@yandex.ru>

---
+++ trunk/ii02520/task_03/src/Source.cpp
@@ -0,0 +1,13 @@
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	cout << "starting python file..." << endl;
+
+	system("python lab3.py");
+
+	cout << "py code run success";
+	return 0;
+}
